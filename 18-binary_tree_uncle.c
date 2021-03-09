15f3b6c62f990d90666de26fe5d7733af7c01a1f
James <jmartinez21834@stu.nuc.edu>
2021-03-09 16:04:10 +0000

    adding task 18 files

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,3 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: If node is NULL or the parent is NULL, return NUL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else if (node == node->parent->right)
+		return (node->parent->left);
+	else
+		return (NULL);
+}
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return:if is uncle or 0 otherwise
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
