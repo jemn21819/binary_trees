1425f052ad2727320376519328204552d0f03953
James <jmartinez21834@stu.nuc.edu>
2021-03-10 16:38:54 +0000

    adding task 110 file

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -41,7 +41,7 @@ int bst_right(const binary_tree_t *tree, int val)
 }
 
 /**
- * binary_tree_is_bst = checks if a binary tree is a valid Binary Search Tree
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is a valid BST, and 0 otherwise
  */
