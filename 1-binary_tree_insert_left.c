a67485c73517c9dbc8e22acfd5b70d3ff82a3191
James <jmartinez21834@stu.nuc.edu>
2021-03-10 12:49:39 +0000

    fixing code

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,11 +11,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (!new)
-		return (NULL);
 	if (!parent)
 		return (NULL);
 	new = binary_tree_node(parent, value);
+	if (!new)
+		return (NULL);
 	new->left = parent->left;
 	if (parent->left)
 		parent->left->parent = new;
