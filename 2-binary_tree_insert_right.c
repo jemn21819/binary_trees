e4f4b681beca7cbbf7be8dd5f6135a8e811be1de
James <jmartinez21834@stu.nuc.edu>
2021-03-10 14:24:38 +0000

    making correction to order of the code

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,10 +11,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	if (!parent)
+		return (NULL);
 	new = binary_tree_node(parent, value);
 	if (!new)
-		return (NULL);
-	if (!parent)
 		return (NULL);
 	new->right = parent->right;
 	if (parent->right)
